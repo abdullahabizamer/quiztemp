b602fec992f5456fadcbb8a7e696f9327df0ce98
abdullahabizamer <photos.hama@gmail.com>
2025-02-24 00:52:35 +0000

    لارفيل

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { tempo } from "tempo-devtools/dist/vite";
 
-const conditionalPlugins: [string, Record<string, any>][] = [];
+const conditionalPlugins = [];
 
 // @ts-ignore
 if (process.env.TEMPO === "true") {
@@ -12,10 +12,6 @@ if (process.env.TEMPO === "true") {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
-  optimizeDeps: {
-    entries: ["src/main.tsx", "src/tempobook/**/*"],
-  },
   plugins: [
     react({
       plugins: conditionalPlugins,
@@ -23,13 +19,12 @@ export default defineConfig({
     tempo(),
   ],
   resolve: {
-    preserveSymlinks: true,
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
-    // @ts-ignore
-    allowedHosts: true,
-  }
+    host: "0.0.0.0",
+    port: 3000,
+  },
 });
